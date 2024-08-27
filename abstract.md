593ba84841139c211f56c03563de28c184159639
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2024-08-27 09:10:38 -0400

    update pandoc convert script

---
+++ abstract.md
@@ -0,0 +1,3 @@
+# Abstract
+
+This article proceeds from the premise that one's geographical context should fundamentally inform how one engages with geospatial data preservation. If a dataset is updated continuously, then it should also be collected continuously.
--- convert.sh
+++ convert.sh
@@ -3,4 +3,5 @@
 dt=$(date '+%d-%m-%Y');
 in="the-snapshot-problem.md"
 out="drafts/the-snapshot-problem_draft_$dt.docx"
-pandoc $in --number-sections --citeproc -o $out
+ref="ref/custom-reference.docx"
+pandoc $in --number-sections --reference-doc=$ref --citeproc -o $out
